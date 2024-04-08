7042d2606ce7b8a8e61366416820ba8d72cb9384
Deepanshurao2001 <raodeepanshu99@gmail.com>
2024-04-08 15:12:40 +0530

    learn how to use another way to create router and loader

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import {useLoaderData} from 'react-router-dom'
+function github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/deepanshurao2001')
+    //     .then(Response => Response.json())
+    //     .then(data => {
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // }, [])
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white'>Github followers : {data.followers}
+     <img  src={data.avatar_url} alt="Git picture" width={300}/>
+    </div>
+   
+  )
+}
+
+export default github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/deepanshurao2001')
+    return response.json()
+}
